2640e708e4da22be30f95d4291124cdb63b16038
Michael McDonald <mjmickd@gmail.com>
2022-07-08 07:31:36 -0700

    commit tree 9cecfd6b18f9fd842a23c37bc1bcdc143caf5d2c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Follow along with the examples here
+function doNothing () {} 
+function sayHello() {
+    console.log("Hello!")
+}
+sayHello()
+function sayHelloToGuadalupe(){
+     console.log("Hello, Guadalupe!")
+}
+sayHelloToGuadalupe()
+function sayHelloToLiz(){
+    console.log("Hello,Liz!")
+}
+sayHelloToLiz()
+function sayHelloToSamip(){
+    console.log("Hello,Samip!")
+}
+sayHelloToSamip()
